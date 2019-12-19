16c4b10164f76af1a0afa4536200a37998ee4bf0
JinwooKing <wlsdn9489@naver.com>
2019-12-19 20:38:15 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
     /* set css*/
-    $('#rap').css('padding','0px 8.5%');
-    $('body').css('background-color','white');
+    $('#rap').css('padding', '0px 8.5%');
+    $('body').css('background-color', 'white');
 
     /* header */
     $('h1').css('font-family', 'BMHANNA_11yrs');
@@ -9,14 +9,28 @@ $(document).ready(function(){
 
 
     /* sidebar*/
-    $('#sidbar').css({'margin':'-1px auto',
-                     'left': '98px'}).children().children() /* sidlist */
-                .css({'width':'300px'
-                      ,'height': '968px'}).children() /* sidebar-nav */
-                .css('padding','0px').children()/* sidebar-brand */
-                .css({'list-style':'none'});
-    $('.sidebar-nav').children().on('click', function() {
+    $('#sidbar').css({
+            'margin': '-1px auto',
+            'left': '98px'
+        }).children().children() /* sidlist */
+        .css({
+            'width': '300px',
+            'height': '968px'
+        }).children() /* sidebar-nav */
+        .css('padding', '0px').children() /* sidebar-brand */
+        .css({
+            'list-style': 'none'
+        });
+    $('.sidebar-nav').children().on('click', function () {
         let ch = $(this).children('ul');
         $(ch).toggle();
     });
+
+    /* figcaption */
+        $(".hover").mouseleave(
+            function() {
+                $(this).removeClass("hover");
+            }
+        );
+
 })
