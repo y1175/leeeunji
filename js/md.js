11d1d69e581cb3e49d4faf845079f769dfbbc23e
Jinny <x_o1215@naver.com>
2019-12-19 20:49:56 +0900

    fkfkfkd

---
+++ js/md.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    @media (min-width: 320px) and (max-width: 480px) {
+
+    $('#page-wrapper').hide();
+    $('#sid').show();
+
+}
+
+@media (min-width: 768px) and (max-width: 1024px) {
+
+    $('#page-wrapper').hide();
+    $('#sid').show();
+
+}
+
+    });
