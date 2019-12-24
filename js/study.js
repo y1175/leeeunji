d59f764eb1b48c0a82ebd34fcda5b0134ffe732d
Jinny <x_o1215@naver.com>
2019-12-24 10:32:15 +0900

    ㅇㅇㅇㅇㅇ

--- js/study.js
+++ js/study.js
@@ -1,17 +1,14 @@
 $(function() {
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 600) {
-            $('.ScrollButton').fadeIn();
+        if ($(this).scrollTop() > 200) { // 얘의 위치가 스크롤된 화면 기준 600을 넘어가면
+            $('.ScrollButton').fadeIn(); // 스크롤 버튼이 생김
         } else {
-            $('.ScrollButton').fadeOut();
+            $('.ScrollButton').fadeOut(); // 아니면 스크롤 버튼이 사라짐
         }
     });
 
     $("#TopButton").click(function() {
-        $('html').animate({scrollTop : 0}, 600);
+        $('html').animate({scrollTop : 0}, 600); // 그리고 눌렀을때 화면의 맨 위로 올라감 600은 올라가는 속도 조절
     });
 
-    $("#BottomButton").click(function() {
-        $('html').animate({scrollTop : ($('#footer').offset().top)}, 600);
-    });
 });
